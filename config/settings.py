8dd95904c0e6a021053f437e6a4f8aac4bba95d2
jseop-lim <jseoplim@gmail.com>
2022-04-01 23:53:58 +0900

    Feat: Change DBMS to MySQL

--- config/settings.py
+++ config/settings.py
@@ -92,12 +92,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = get_secret("DATABASES")
 
 
 # Password validation
