7d7853b39a59c7d4e829ddc6b7bc0beb5026e9ff
jseop-lim <jseoplim@gmail.com>
2022-04-30 23:40:40 +0900

    Refactor: Modify sell, buy, bookmark views
    `queryset = User.objects.all()` 대신 `get_queryset()`을 정의
    `get_object_or_404()`를 직접 호출
    `list()` 삭제

--- mypage/views.py
+++ mypage/views.py
@@ -2,7 +2,7 @@ from mypage.models import User
 from mypage.serializers import UserSerializer, TicketListSerializer, BuySerializer, BookmarkSerializer
 
 from rest_framework import generics
-from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 
 
 class UserDetailView(generics.RetrieveAPIView):
@@ -17,54 +17,33 @@ class SellView(generics.ListAPIView):
     """
     판매내역
     """
-    queryset = User.objects.all()
     serializer_class = TicketListSerializer
 
-    def list(self, request, *args, **kwargs):
-        queryset = self.get_object().sell_tickets
-
-        page = self.paginate_queryset(queryset)
-        if page is not None:
-            serializer = self.get_serializer(page, many=True)
-            return self.get_paginated_response(serializer.data)
-
-        serializer = self.get_serializer(queryset, many=True)
-        return Response(serializer.data)
+    def get_queryset(self):
+        pk = self.kwargs.get('pk')
+        user = get_object_or_404(User, pk=pk)
+        return user.sell_tickets
 
 
 class BuyView(generics.ListAPIView):
     """
     구매내역
     """
-    queryset = User.objects.all()
     serializer_class = BuySerializer
 
-    def list(self, request, *args, **kwargs):
-        queryset = self.get_object().buys
-
-        page = self.paginate_queryset(queryset)
-        if page is not None:
-            serializer = self.get_serializer(page, many=True)
-            return self.get_paginated_response(serializer.data)
-
-        serializer = self.get_serializer(queryset, many=True)
-        return Response(serializer.data)
+    def get_queryset(self):
+        pk = self.kwargs.get('pk')
+        user = get_object_or_404(User, pk=pk)
+        return user.buys
 
 
 class BookmarkView(generics.ListAPIView):
     """
     관심상품
     """
-    queryset = User.objects.all()
     serializer_class = BookmarkSerializer
 
-    def list(self, request, *args, **kwargs):
-        queryset = self.get_object().bookmarks
-
-        page = self.paginate_queryset(queryset)
-        if page is not None:
-            serializer = self.get_serializer(page, many=True)
-            return self.get_paginated_response(serializer.data)
-
-        serializer = self.get_serializer(queryset, many=True)
-        return Response(serializer.data)
+    def get_queryset(self):
+        pk = self.kwargs.get('pk')
+        user = get_object_or_404(User, pk=pk)
+        return user.bookmarks
