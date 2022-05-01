7874d09e1bee25a750624d9c7d9f8d8dfb444f37
jseop-lim <jseoplim@gmail.com>
2022-05-02 02:11:47 +0900

    Refactor: Separate serializers.py and chore change
    mypage/serializers 디렉토리 생성, *_serializers.py를 아래로 이동
    - serializers.py의 이름을 user_serializers.py로 변경
    - 새로운 파일 ticket_serializers.py 생성
    
    config/urls.py에서 mypage.urls의 path를 ''로 변경
    mypage url path 앞에 'users/' 추가
    
    user serializers, user views 클래스 이름 앞에 User 추가
    
    (#8)

--- mypage/urls.py
+++ mypage/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from mypage.views import user_views
+from mypage.views import user_views, ticket_views
 
 app_name = 'mypage'
 
 
 urlpatterns = [
-    path('<int:pk>', user_views.UserDetailView.as_view(), name='user-detail'),
-    path('<int:pk>/bookmarks', user_views.BookmarkView.as_view(), name='user-bookmark'),
-    path('<int:pk>/buys', user_views.BuyView.as_view(), name='user-buy'),
-    path('<int:pk>/sells', user_views.SellView.as_view(), name='user-sell'),
+    path('users/<int:pk>', user_views.UserDetailView.as_view(), name='user-detail'),
+    path('users/<int:pk>/bookmarks', user_views.UserBookmarkView.as_view(), name='user-bookmark'),
+    path('users/<int:pk>/buys', user_views.UserBuyView.as_view(), name='user-buy'),
+    path('users/<int:pk>/sells', user_views.UserSellView.as_view(), name='user-sell'),
 ]
