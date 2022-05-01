678e97b587573cd837be28539ff99e5bccb344e2
jseop-lim <jseoplim@gmail.com>
2022-05-02 01:11:39 +0900

    Refactor: Separate the file views.py
    mypage/views 디렉토리 생성, *_views.py를 아래로 이동
    - views.py의 이름을 user_views.py로 변경
    - 새로운 파일 ticekt_views.py 생성
    urls.py에서 import path 수정
    
    (#8)

--- mypage/urls.py
+++ mypage/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from mypage.views import UserDetailView, SellView, BuyView, BookmarkView
+from mypage.views import user_views
 
 app_name = 'mypage'
 
 
 urlpatterns = [
-    path('<int:pk>', UserDetailView.as_view(), name='user-detail'),
-    path('<int:pk>/bookmarks', BookmarkView.as_view(), name='user-bookmark'),
-    path('<int:pk>/buys', BuyView.as_view(), name='user-buy'),
-    path('<int:pk>/sells', SellView.as_view(), name='user-sell'),
+    path('<int:pk>', user_views.UserDetailView.as_view(), name='user-detail'),
+    path('<int:pk>/bookmarks', user_views.BookmarkView.as_view(), name='user-bookmark'),
+    path('<int:pk>/buys', user_views.BuyView.as_view(), name='user-buy'),
+    path('<int:pk>/sells', user_views.SellView.as_view(), name='user-sell'),
 ]
