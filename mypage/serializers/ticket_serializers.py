23af50dc035e1fb32af1cbf969ea8e3b7fd39dcc
jseop-lim <jseoplim@gmail.com>
2022-06-04 16:21:44 +0900

    Feat: Drop list action to buy and bookmark views
    BuyCreateView -> BuyListView: list 제거
    BookmarkCreateView -> BookmarkListView: list 제거

--- mypage/serializers/ticket_serializers.py
+++ mypage/serializers/ticket_serializers.py
@@ -29,7 +29,7 @@ class BuyDetailSerializer(serializers.ModelSerializer):
         fields = ['id', 'user', 'ticket', 'date']
         extra_kwargs = {
             'id': {'help_text': 'Buy ID'},
-            'data': {'required': True},
+            'date': {'required': True},
         }
 
 
