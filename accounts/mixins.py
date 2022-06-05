38012fff19d5abdda91b3fca11e6e3cefcd3e046
jseop-lim <jseoplim@gmail.com>
2022-06-05 15:36:07 +0900

    Feat: Modify kakao login view
    social user가 없으면 401 코드 및 uid, nickname 반환
    반환하는 user id의 모델을 SocialUser로 바꿈
    social service에서 받은 common model이 required_common_fields에 존재
    하지 않으면 에러 반환

--- accounts/mixins.py
+++ accounts/mixins.py
@@ -9,20 +9,6 @@ from django.http import JsonResponse
 
 
 class SocialLoginMixin:
-    def get_user_objects(self, uid):
-        """
-        소셜 서비스 인증 서버에서 받은 uid를 이용해 social_user 객체를 추출하여 반환.
-        새로운 사용자의 경우 social_user와 app_user를 생성.
-        """
-        try:
-            social_user = SocialUser.objects.get(provider=self.provider, uid=uid)
-            app_user = social_user.app_user
-            return social_user, app_user
-        except SocialUser.DoesNotExist:  # 신규 회원일 때
-            social_user = SocialUser.objects.create_user(provider=self.provider, uid=uid)
-            app_user = AppUser.objects.create(social_user=social_user)
-            return social_user, app_user
-
     def create_token(self, social_user):
         """
         Access Token 발행
@@ -37,18 +23,19 @@ class SocialLoginMixin:
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
 
-        uid = serializer.validated_data['id']
-        social_user, app_user = self.get_user_objects(uid)
-        access_token, refresh_token = self.create_token(social_user)
-
-        data = {
-            'access_token': access_token,
-            'refresh_token': refresh_token,
-            'user': app_user,
-        }
-        response_data = JWTSerializer(data).data
-
-        return JsonResponse(response_data, status=status.HTTP_200_OK)
+        uid = serializer.validated_data['uid']
+        try:
+            social_user = SocialUser.objects.get(provider=self.provider, uid=uid)
+        except SocialUser.DoesNotExist:  # 신규 회원일 때 TODO 필드 직접 입력하기, nickname 없을 경우 에러 반환
+            return JsonResponse(data=serializer.validated_data, status=status.HTTP_401_UNAUTHORIZED)
+        else:
+            access_token, refresh_token = self.create_token(social_user)
+            data = {
+                'access_token': access_token,
+                'refresh_token': refresh_token,
+                'user': social_user,
+            }
+            return JsonResponse(JWTSerializer(data).data, status=status.HTTP_200_OK)
 
 
 class LogoutMixin:
--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,26 +1,16 @@
-from mypage.models import User
-
-import requests
+from accounts.models import SocialUser
+from mypage.models import User  # TODO TEMP
+from mypage.serializers.user_serializers import UserCreateSerializer
 
 from rest_framework import serializers
-
-
-class JWTUserSerializer(serializers.ModelSerializer):
-    id = serializers.IntegerField(source='social_user.id', help_text='User ID')
-
-    class Meta:
-        model = User
-        fields = ['id']
-
-
-class JWTSerializer(serializers.Serializer):
-    access_token = serializers.CharField(help_text='Baton App 인증을 위한 Access Token')
-    refresh_token = serializers.CharField(help_text='Baton App Access Token 갱신을 위한 Refresh Token')
-    user = JWTUserSerializer()
+from rest_framework.exceptions import ValidationError
+from django.utils.translation import gettext as _
+import requests
 
 
 class SocialLoginSerializer(serializers.Serializer):
     access_token = serializers.CharField(required=True, help_text='Kakao 서버 인증을 위한 Access Token')
+    required_common_fields = ['nickname']
     profile_url = ''
 
     class Meta:
@@ -33,14 +23,26 @@ class SocialLoginSerializer(serializers.Serializer):
         raise NotImplementedError
 
     def validate(self, attrs):
-        # 카카오 인증 서버에 접속 -> 사용자 프로필 정보 알아옴
+        # 소셜 서비스 인증 서버에 접속 -> 사용자 프로필 정보 알아옴
         headers = {"Authorization": f"Bearer {attrs['access_token']}"}
         response = requests.get(self.profile_url, headers=headers)
         response.raise_for_status()
         extra_data = response.json()
-        attrs['id'] = self.extract_uid(extra_data)
-        # attrs.update(self.extract_common_fields(extra_data))
+        attrs['uid'] = self.extract_uid(extra_data)
+        attrs.update(self.extract_common_fields(extra_data))
+        self.validate_common_fields(attrs)
         return attrs
+
+    def validate_common_fields(self, attrs):
+        # all(field in self.required_common_fields for field in attrs)
+        errors = []
+
+        for field in self.required_common_fields:
+            if not attrs.get(field, None):
+                errors.append({field: [_('This field is required.')]})
+
+        if errors:
+            raise ValidationError(errors)
 
 
 class KaKaoLoginSerializer(SocialLoginSerializer):
@@ -50,8 +52,53 @@ class KaKaoLoginSerializer(SocialLoginSerializer):
         return str(data['id'])
 
     def extract_common_fields(self, data):
-        email = data.get("kakao_account", {}).get("email")
-        nickname = data.get("properties", {}).get("nickname")
+        common_fields = {
+            # 'email': data.get("kakao_account", {}).get("email"),
+            'nickname': data.get("properties", {}).get("nickname"),
+        }
+        return common_fields
 
-        return dict(email=email, nickname=nickname)
 
+class JWTUserSerializer(serializers.ModelSerializer):
+    """
+    로그인 성공 시 반환하는 사용자 정보
+    """
+
+    class Meta:
+        model = SocialUser
+        fields = ['id']
+        extra_kwargs = {
+            'id': {'help_text': 'User ID'},
+        }
+
+
+class JWTSerializer(serializers.Serializer):
+    """
+    로그인 성공 시 반환하는 JWT 토큰의 내용
+    """
+    access_token = serializers.CharField(help_text='Baton App 인증을 위한 Access Token')
+    refresh_token = serializers.CharField(help_text='Baton App Access Token 갱신을 위한 Refresh Token')
+    user = JWTUserSerializer()
+
+
+class SocialUserCreateSerializer(serializers.ModelSerializer):
+    """
+    회원가입 요청 시 전달 받는 데이터
+    """
+    user = UserCreateSerializer()
+
+    class Meta:
+        model = SocialUser
+        fields = ['uid', 'user']
+
+    def create(self, validated_data):
+        # create social_user
+        try:
+            user_data = validated_data.pop('user')
+        except IndexError:
+            raise IndexError('사용자 정보 미포함')
+        else:
+            social_user = SocialUser.objects.create_user(provider=self.provider, uid=validated_data['uid'])
+            app_user = User.objects.create(id=social_user, **user_data)
+
+        return social_user
