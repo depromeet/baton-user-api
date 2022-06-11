56e52dcdf3dad51019ad33028103ae4bcf9cd71f
jseop-lim <jseoplim@gmail.com>
2022-06-11 19:34:38 +0900

    Feat: Make user update APIs
    user 기본정보, 주소, 계좌 수정 API 추가

--- user-api/mypage/urls.py
+++ user-api/mypage/urls.py
@@ -5,6 +5,8 @@ from mypage.views import user_views, ticket_views
 urlpatterns = [
     path('users', user_views.UserCreateView.as_view(), name='user-create'),
     path('users/<int:pk>', user_views.UserDetailView.as_view(), name='user-detail'),
+    path('users/<int:user>/account', user_views.UserAccountView.as_view(), name='user-account'),
+    path('users/<int:pk>/address', user_views.UserAddressView.as_view(), name='user-address'),
     path('users/<int:pk>/bookmarks', user_views.UserBookmarkView.as_view(), name='user-bookmark'),
     path('users/<int:pk>/buys', user_views.UserBuyView.as_view(), name='user-buy'),
     path('users/<int:pk>/sells', user_views.UserSellView.as_view(), name='user-sell'),
