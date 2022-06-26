2b91eb20827fdf29ab65cbd75e2bff5a2ebbcae8
jseop-lim <jseoplim@gmail.com>
2022-06-27 01:47:34 +0900

    Feat: Upload static files of auth-server
    django-storages, boto3 패키지 설치
    settings에서 INSTALLED_APPS에 storages 추가

--- auth-server/config/settings/base.py
+++ auth-server/config/settings/base.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     # 'rest_framework_simplejwt.token_blacklist',
+    'storages',
     # swagger
     'drf_yasg',
     # proxy-server
